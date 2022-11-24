393ee4efd7e21e2f0b5fca82fffa0600319e6641
AskaryanKarine <askariyan.karine@gmail.com>
2022-11-24 23:23:16 +0300

    added picture's methods

--- src/picture/picture.cpp
+++ src/picture/picture.cpp
@@ -4,9 +4,21 @@ Picture::Picture()
 {
 }
 
+Picture::Picture(int height, int width, int depth)
+    : _height(height)
+    , _width(width)
+    , _maxDepth(depth)
+{
+}
+
 void Picture::set_height(int height)
 {
     _height = height;
+}
+
+int Picture::get_height()
+{
+    return _height;
 }
 
 void Picture::set_width(int width)
@@ -14,7 +26,44 @@ void Picture::set_width(int width)
     _width = width;
 }
 
+int Picture::get_width()
+{
+    return _width;
+}
+
 void Picture::set_maxDepth(int max_depth)
 {
     _maxDepth = max_depth;
+}
+
+int Picture::get_maxDepth()
+{
+    return _maxDepth;
+}
+
+void Picture::set_camera(Camera cam)
+{
+    _cam = cam;
+}
+
+Camera Picture::get_camera()
+{
+    return _cam;
+}
+
+std::shared_ptr<QImage> Picture::drawingFgure()
+{
+    _cam = Camera();
+    _cam.set_direction(QVector3D(0, 0, 3000));
+
+    std::shared_ptr<QImage> image = std::make_shared<QImage>(_width, _height, QImage::Format_RGB32);
+    image->fill(Qt::black);
+
+    for (int y = 0; y < _height; y++) {
+        for (int x = 0; x < _width; x++) {
+            QVector3D pix(x, y, 200);
+        }
+    }
+
+    return image;
 }
--- src/picture/picture.h
+++ src/picture/picture.h
@@ -1,25 +1,48 @@
 #ifndef PICTURE_H
 #define PICTURE_H
 
+#include "baselight.h"
+#include "camera.h"
+#include "model.h"
+
 #include <QImage>
+#include <QVector3D>
 #include <memory>
 
 class Picture {
 public:
     Picture();
+    Picture(int height, int width, int depth);
 
     void set_height(int height);
+    int get_height();
+
     void set_width(int width);
+    int get_width();
+
     void set_maxDepth(int max_depth);
+    int get_maxDepth();
+
+    void set_camera(Camera cam);
+    Camera get_camera();
+
+    //    void set_object
+
+    //    QVector3D cast_ray();
+
+    std::shared_ptr<QImage> drawingFgure();
+
+    std::shared_ptr<QImage> drawingFigure(int nThr);
+    void drawThr(int start, int end, std::shared_ptr<QImage>& img);
 
 private:
     int _height;
     int _width;
     int _maxDepth;
 
-    //    std::vector<Model> _figures;
-    //    std::vector<BaseLight> _lights;
-    //    Camera _cam;
+    std::vector<Model> _figures;
+    std::vector<BaseLight> _lights;
+    Camera _cam;
 };
 
 #endif // PICTURE_H
