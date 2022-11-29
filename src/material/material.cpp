b788395e4dd7725374ae4054eb13e7afabb41deb
AskaryanKarine <askariyan.karine@gmail.com>
2022-11-29 03:04:46 +0300

    added material class

--- src/material/material.cpp
+++ src/material/material.cpp
@@ -2,5 +2,63 @@
 
 Material::Material()
 {
+}
 
+Material::Material(double r, double spec, QVector4D alb, QColor difcol)
+    : _refractive_index(r)
+    , _specular_exponent(spec)
+    , _albedo(alb)
+    , _difColor(difcol)
+    , _specColor(1, 1, 1)
+{
+}
+
+double Material::get_refractive_index()
+{
+    return _refractive_index;
+}
+
+void Material::set_refractive_index(double r)
+{
+    _refractive_index = r;
+}
+
+double Material::get_spec_exp()
+{
+    return _specular_exponent;
+}
+
+void Material::set_spec_exp(double spec)
+{
+    _specular_exponent = spec;
+}
+
+QVector4D Material::get_albedo()
+{
+    return _albedo;
+}
+
+void Material::set_albedo(QVector4D alb)
+{
+    _albedo = alb;
+}
+
+QColor Material::get_difColor()
+{
+    return _difColor;
+}
+
+void Material::set_difColor(QColor difCol)
+{
+    _difColor = difCol;
+}
+
+QColor Material::get_specColor()
+{
+    return _specColor;
+}
+
+void Material::set_specColor(QColor specCol)
+{
+    _specColor = specCol;
 }
--- src/material/material.h
+++ src/material/material.h
@@ -1,11 +1,34 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include <QColor>
+#include <QVector4D>
 
-class Material
-{
+class Material {
 public:
     Material();
+    Material(double r, double spec, QVector4D alb, QColor difcol);
+
+    double get_refractive_index();
+    void set_refractive_index(double r);
+
+    double get_spec_exp();
+    void set_spec_exp(double spec);
+
+    QVector4D get_albedo();
+    void set_albedo(QVector4D alb);
+
+    QColor get_difColor();
+    void set_difColor(QColor difCol);
+
+    QColor get_specColor();
+    void set_specColor(QColor specCol);
+
+private:
+    double _refractive_index, _specular_exponent;
+    QVector4D _albedo;
+    QColor _difColor;
+    QColor _specColor;
 };
 
 #endif // MATERIAL_H
