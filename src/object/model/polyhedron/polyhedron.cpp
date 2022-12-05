b2b2307a23b7fcdaadcce6e7d2f0a9580c9e0d8b
AskaryanKarine <askariyan.karine@gmail.com>
2022-12-05 13:59:33 +0300

    added implementation polyhedron class

--- src/object/model/polyhedron/polyhedron.cpp
+++ src/object/model/polyhedron/polyhedron.cpp
@@ -2,5 +2,13 @@
 
 Polyhedron::Polyhedron()
 {
+}
 
+void Polyhedron::transform(const QVector3D& move, const QVector3D& scale, const QVector3D& rotate)
+{
+}
+
+bool Polyhedron::rayIntersect(const QVector3D& origin, const QVector3D& direction, double& t, QVector3D& norm)
+{
+    return true;
 }
--- src/object/model/polyhedron/polyhedron.h
+++ src/object/model/polyhedron/polyhedron.h
@@ -2,16 +2,20 @@
 #define POLYHEDRON_H
 
 #include "model.h"
+#include "polygon.h"
 
-class Polyhedron {
+class Polyhedron : public Model {
 public:
     Polyhedron();
+    ~Polyhedron() {};
 
-    //    void transform(const QVector3D& move, const QVector3D& scale, const QVector3D& rotate) override;
+    void transform(const QVector3D& move, const QVector3D& scale, const QVector3D& rotate) override;
+    bool rayIntersect(const QVector3D& origin, const QVector3D& direction, double& t, QVector3D& norm) override;
 
 private:
     QVector3D _center;
     std::vector<QVector3D> _points;
+    std::vector<Polygon> _pols;
 };
 
 #endif // POLYHEDRON_H
